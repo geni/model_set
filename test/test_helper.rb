370012955aabac6b5d72a9c6f0d0f4e3f03fd2ac
Scott Steadman <scott.steadman@geni.com>
2025-05-01 10:59:22 -0700

    remove CaptureRubyWarnings extension
      The noise left once we updated the gems.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,6 @@
 require 'test/unit'
 require 'pp'
 
-module CaptureRubyWarnings
-  def warn(message)
-    return if message =~ /assigned but unused variable/
-    return if caller[0] =~ /vendor/ # Ignore warnings from vendored code
-    super
-  end
-end
-
-Warning.extend(CaptureRubyWarnings)
-
 unless defined?($SKIP_COVERAGE)
   require 'simplecov'
   SimpleCov.start do
