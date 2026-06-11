c746aaa3983250d5cd5e0964d940e64c9fe9fdef
Mike Stangel <mike@geni.com>
2026-06-11 11:29:13 -0700

    test script

---
+++ test_my_app.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+bundle='bundle _1.17.3_'
+
+#jbundle config --local build.pg "--with-pg-config=/usr/pgsql-10/bin/pg_config"
+$bundle config --local build.sqlite3 "--enable-system-libraries"
+$bundle config --local clean true
+$bundle config --local path vendor/bundle
+$bundle config --local without vscode
+
+rm Gemfile.lock
+$bundle install
+
+#rm db/test.sqlite3
+$bundle exec rake test
+
